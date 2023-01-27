206465d42cd48f85ceda29015f773eb94f97b8c6
Pickorea <38229544+Pickorea@users.noreply.github.com>
2023-01-27 16:43:31 +1200

    comment

--- app/Http/Controllers/AlertSystem/NotifyController.php
+++ app/Http/Controllers/AlertSystem/NotifyController.php
@@ -22,98 +22,10 @@ class NotifyController  extends Controller {
 	 */
 	public function index() {
 
-		// $employees = Employee::all();
+		$employees = Employee::select('email')->get();
 		// User::find(1)->notify(new WorkStatusAlert);
-			\Notification::route('mail', 'kairaoi1ien@yahoo.com')->notify(new WorkStatusAlert);
+			\Notification::route('mail', $employees)->notify(new WorkStatusAlert);
 		return view('alertsystems.employees.index', compact('employees'));
-	}
-
-	
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('alertsystems.employees.create');
-    }
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function store(Request $request) {
-		
-		            $input = $request->all();
-	
-					$results = Employee::create($input);
-
-			
-				return redirect()->route('employee.index');
-	}
-
-	
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function show($id) {
-	
-		$employee = Employee::find($id);
-
-		return view('alertsystems.employees.show')
-	        ->with('employee',$employee);
-
-	}
-
-	
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function edit($id) {
-		
-        $employee = Employee::find($id)->toArray();
-		return view('alertsystems.employees.edit')->withEmployee($employee);
-        
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function update(Request $request, $id){
-        
-          $employee = $request->all();
-         $data = Employee::find($id)->update($employee);
-
-
-			return redirect()->route('employee.index')->with('message', 'Updated successfully.');
-	
-    }
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function destroy($id) {
-		
-		return redirect()->route('/employee')->with('exception', 'Operation failed !');
 	}
 
 	
